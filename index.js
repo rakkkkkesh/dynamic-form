c7758ea72a6762af1735e3ee88daa938ed3c21a6
Rakesh Yadav <rakeshpyadav2615@gmail.com>
2023-10-01 19:00:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function fetch(){
+    let name = document.getElementById("Name").value
+    var email = document.getElementById("Email").value
+    var password = document.getElementById("Password").value
+    var branch = document.getElementById("Branch").value
+    document.writeln("Name:"+name)
+    document.writeln("Email:"+email)
+    document.writeln("Password:"+password)
+    document.writeln("Branch:"+branch)
+
+}
